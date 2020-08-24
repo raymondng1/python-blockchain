d903c92fdd345a77361cace215fd36d52bac5c04
Raymond Ng <raymondng@Raymonds-MacBook-Air.local>
2020-08-23 21:41:41 -0400

    mine blocks on block-chain

--- block.py
+++ block.py
@@ -1,17 +1,49 @@
+import time
+
+def mine_block(last_block, data):
+    """
+    Mine a block based on the give last_block and data.
+    """
+    timestamp = time.time_ns()
+    last_hash = last_block.hash
+    hash = f'{timestamp} -{last_hash}'
+
+    return Block(timestamp, last_hash, hash, data)
+
+def genesis():
+    """
+    Generate gensis block.
+    """
+    return Block(1, 'genesis_last_hash' , 'genesis_hash', [])
+
 class Block:
     """
     Block: a unit storage.
     Store transactions in a blockchain that supports a cryptocurrency.
     """
-    def __init__(self, data):
+    def __init__(self, timestamp, last_hash, hash, data):
+        self.timestamp = timestamp
+        self.last_hash = last_hash
+        self.hash = hash
         self.data = data
+    
     def __repr__(self): 
-        return f'Block - data: {self.data}'
+        return (
+            'Block('
+            f'timestamp: {self.timestamp}, '
+            f'last_hash: {self.last_hash}, '
+            f'hash:{self.hash},'
+            f'Block - data: {self.data}'
+        )
 
 def main():
-    block = Block('foo')
+    # block = Block('foo')
+    # print(block)
+    # print(f'block.py __name__: {__name__}')
+
+    genesis_block = genesis()
+    block = mine_block(genesis_block, 'foo')
     print(block)
-    print(f'block.py __name__: {__name__}')
 
 if __name__ == '__main__':
     main()
