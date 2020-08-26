00e9a287d9502357834933f7c277cc3568de3fd9
Raymond Ng <raymond.ng47@gmail.com>
2020-08-26 04:36:42 -0400

    add testing for block and  blockchain

--- README.md
+++ README.md
@@ -10,3 +10,12 @@ source blockahin-env/bin/activate
 ```
 pip3 install -r requirements.txt
 ```
+
+**Run the tests**
+
+Make sure to activate the virtual environment.
+
+```
+source blockchain-env/bin/activate
+python3 -m pytest backend/tests
+```
--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -1,6 +1,13 @@
 import time
 
-from crypto_hash import crypto_hash
+from backend.util.crypto_hash import crypto_hash
+
+GENESIS_DATA = {
+    'timestamp': 1,
+    'last_hash': 'genesis_last_hash',
+    'hash': 'genesis_hash',
+    'data': []
+}
 
 class Block:
     """
@@ -37,7 +44,13 @@ class Block:
         """
         Generate gensis block.
         """
-        return Block(1, 'genesis_last_hash' , 'genesis_hash', [])
+        return Block(**GENESIS_DATA)
+        # return Block(
+        #     GENESIS_DATA['timestamp'],
+        #     GENESIS_DATA['last_hash'],
+        #     GENESIS_DATA['hash'],
+        #     GENESIS_DATA['data']
+        #     )
 
 def main():
     # block = Block('foo')
