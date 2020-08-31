c3540c5b1e7b13abc41d81ebf93b7961a2971ee9
Raymond Ng <raymond.ng47@gmail.com>
2020-08-30 21:56:38 -0400

    dynamic difficulty and mine rate

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -1,6 +1,7 @@
 import time
 
 from backend.util.crypto_hash import crypto_hash
+from backend.config import MINE_RATE
 
 GENESIS_DATA = {
     'timestamp': 1,
@@ -41,13 +42,14 @@ class Block:
         """
         timestamp = time.time_ns()
         last_hash = last_block.hash
-        difficulty = last_block.difficulty
+        difficulty = Block.adjust_difficulty(last_block, timestamp)
         nonce = 0
         hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
 
         while hash[0:difficulty] != '0' * difficulty:
             nonce += 1
             timestamp = time.time_ns()
+            difficulty = Block.adjust_difficulty(last_block, timestamp)
             hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
 
         return Block(timestamp, last_hash, hash, data, difficulty, nonce)
@@ -64,6 +66,20 @@ class Block:
         #     GENESIS_DATA['hash'],
         #     GENESIS_DATA['data']
         #     )
+    @staticmethod
+    def adjust_difficulty(last_block, new_timestamp):
+        """
+        Caculate the adjusted difficulty according to the MINE_RATE.
+        Increase the difficulty for quickly mined blocks.
+        Decrease the difficulty for slowly mined blocks
+        """
+        if (new_timestamp - last_block.timestamp) < MINE_RATE:
+            return last_block.difficulty + 1
+        
+        if (last_block.difficulty - 1) > 0:
+            return last_block.difficulty - 1
+
+        return 1
 
 def main():
     # block = Block('foo')
